b85a24c5e5f3027ea2d6f9669b31ade4019123df
Aleyda Davalos <aledavalos@Aleydas-MacBook-Pro.local>
2021-11-20 10:27:01 -0800

    server changes

--- server/server.js
+++ server/server.js
@@ -5,4 +5,28 @@ const PORT = 3000;
 const cors = require ('cors');
 
 //import from db
-const seed
+const seed = require ('./seed');
+const {db} = require ('./db');
+const {User} = require ('./Models/user');
+const {Post} = require ('./Models/post');
+const {findAllInRenderedTree} = require ('eadt-dom/test-utils')
+
+//invoking our seed function
+seed()
+
+//EXPRESS function that will allow us to make HTTP requests
+app.use(express.json())
+
+app.use(cors())//allows us to use an external API
+
+/*******Routes*********** */
+app.get('/', async(req, res) => {
+    console.log("THIS IS THE 200 Message");
+    res.send(200)
+})
+
+/****************************** */
+app.listen(PORT, ()=>{
+    console.log(`Your server is now listening to port ${PORT}`)
+})
+
